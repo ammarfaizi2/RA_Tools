513e80877375a59eedbf0e2c39f3a091207322ee
U0_a147 <ammarfaizi2@gmail.com>
2017-04-10 08:59:32 +0700

    Fix little shit

--- git_dw.php
+++ git_dw.php
@@ -3,20 +3,20 @@ date_default_timezone_set("Asia/Jakarta");
 header("content-type:application/json");
 class git_dw
 {
-	const dgit = __DIR__."/.git_dw/";
+	const dgit = ".git_dw/";
 	private $master;
 	public function __construct($url)
 	{
 		if(!isset($_COOKIE['auth'],$_COOKIE['key'])){
-			exit();
+			exit($this->err("Auth failed"));
 		}
 		is_dir(self::dgit) or mkdir(self::dgit);
 		is_dir(self::dgit.'files') or mkdir(self::dgit.'files');
-		if(file_exists(self::dgit.'data.json')){
+		if(!file_exists(self::dgit.'data.json')){
 			file_put_contents(self::dgit.'data.json',json_encode(array("author"=>$_COOKIE['auth'],"first_make"=>(date("Y-m-d H:i:s")),"commit"=>0,"last_commit"=>null)));
 			
 		}
-		if(file_exists(self::dgit.'/pclzip.lib.php')){
+		if(!file_exists(self::dgit.'/pclzip.lib.php')){
 		$a =	$this->download("https://raw.githubusercontent.com/ammarfaizi2/RA_Tools/master/pclzip.lib.php");
 		if($a[0]){
 			exit($this->err("Error download lib : ".$a[0]." ".$a[1]));
@@ -41,9 +41,9 @@ $this->data['author'] = $_COOKIE['auth'];
 $this->data['last_commit_at'] = date("Y-m-d H:i:s");
 $this->data['last_commit'] = $hash;
 file_put_contents(self::dgit.'data.json',json_encode($this->data));
-require self::dgit.'pclzip.lib.php';
-$e = new PclZip(self::dgit.'files/aa_'.(++$this->data['commit']).'.zip');
-$zx = $e->extract(PCLZIP_OPT_PATH,$r,PCLZIP_OPT_REMOVE_PATH,$r);
+require realpath(self::dgit).'/pclzib.lib.php';
+$e = new PclZip(self::dgit.'files/aa_'.($this->data['commit']).'.zip');
+$zx = $e->extract(PCLZIP_OPT_PATH,self::dgit.'..',PCLZIP_OPT_REMOVE_PATH,self::dgit.'..');
 $zx==0 and exit(
 $this->err("Error on extract : ".$e->errorInfo(true))
 );
@@ -68,18 +68,27 @@ exit($this->msg($this->data));
 		CURLOPT_CONNECTTIMEOUT=>500,
 		CURLOPT_COOKIE=>self::dgit.'.git_cookie',
 		CURLOPT_COOKIEJAR=>self::dgit.'.git_cookie',
-		CURLOPT_FOLLOWLOCATION=>true
+		CURLOPT_FOLLOWLOCATION=>false
 		);
 		curl_setopt_array($ch,$op);
 		$out = curl_exec($ch);
 		$err = curl_error($ch) and $out = $err;
 		$ern = curl_errno($ch);
+		$info = curl_getinfo($ch);
+		if(isset($info['redirect_url']) and !empty($info['redirect_url'])){
+			curl_close($ch);
+			$ch = curl_init($info['redirect_url']);
+			curl_setopt_array($ch,$op);
+			$out = curl_exec($ch);
+		$err = curl_error($ch) and $out = $err;
+		$ern = curl_errno($ch);
+		}
 		curl_close($ch);
 		return array($ern,$out);
 	}
 }
 if(!isset($_POST['url'])){
-	die();
+	die("no post");
 }
 $app = new git_dw(trim($_POST['url']));
 $app->run();
